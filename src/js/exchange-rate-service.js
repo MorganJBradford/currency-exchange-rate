b69e0b994b9b8dd82050e28f09da8158bbe8b9cc
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-12 17:50:25 -0600

    define clear fields(); add inputCurrencyCode as api call argument

--- src/js/exchange-rate-service.js
+++ src/js/exchange-rate-service.js
@@ -1,7 +1,7 @@
 export default class ExchangeRateService {
-  static async getCurrencyRate(){
+  static async getCurrencyRate(inputCurrencyCode){
     try{
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${inputCurrencyCode}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
--- src/main.js
+++ src/main.js
@@ -4,31 +4,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from './js/exchange-rate-service.js';
 
+function clearFields() {
+  $('.show-errors').text('');
+  $('#output').text('');
+  
+}
 
 function outputExchangeRate(response, desiredCurrencyCode, inputCurrency) {
-  console.log(inputCurrency);
   if (response.conversion_rates[desiredCurrencyCode] === undefined) {
     $('.show-errors').text(`Sorry, but "${response.conversion_rates[desiredCurrencyCode]}" is not a valid currency`);
     return;
   } else if (isNaN(inputCurrency)) {
     $('.show-errors').text(`Please enter a number:`);
   } else if (response.result) {
-    $('#output').append(`<p>The exchange rate for $${inputCurrency} to ${desiredCurrencyCode} is ${inputCurrency * response.conversion_rates[desiredCurrencyCode]} ${desiredCurrencyCode}</p>`);  
+    $('#output').append(`<p>The exchange rate for ${inputCurrency} is ${inputCurrency * response.conversion_rates[desiredCurrencyCode]} ${desiredCurrencyCode}</p>`);  
   } else {
     $('.show-errors').text(`There was an error: ${response}`);
   }
 }
 
 
-async function exchangeApiCall(desiredCurrencyCode, inputCurrency) {
-  const response = await ExchangeRateService.getCurrencyRate();
+async function exchangeApiCall(inputCurrencyCode, desiredCurrencyCode, inputCurrency) {
+  const response = await ExchangeRateService.getCurrencyRate(inputCurrencyCode);
   outputExchangeRate(response, desiredCurrencyCode, inputCurrency);
 }
 
 $(document).ready(function(){
   $('#rate-checker').click(function(){
+    clearFields();
+    let inputCurrencyCode = $('#convert-from').val();
     let inputCurrency = parseFloat($('#currency-to-convert').val());
     let desiredCurrencyCode = $('#desired-currency').val();
-    exchangeApiCall(desiredCurrencyCode, inputCurrency);
+    exchangeApiCall(inputCurrencyCode, desiredCurrencyCode, inputCurrency);
   });
 });
