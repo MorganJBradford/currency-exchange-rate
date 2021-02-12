b6db0369c10dad108b8746499fc5c34d3c9694fd
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-12 10:33:08 -0600

    define getExchangeRate

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import './css/styles.css';
+import ExchangeRateService from './js/exchange-rate-service';
+
+function getExchangeRate(response) {
+  if (response.result){
+    $('#output').text(`The exchange rate from USD to ${desiredCurrencyCode} is ${response.conversion_rates}.${desiredCurrencyCode}`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
